fc445fb5b0cad9e1dc60a62eb0eb25b807c32561
raob1794 <110286388+raob1794@users.noreply.github.com>
2022-08-03 17:31:17 +0800

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-app.get("/", function (req,res) {
-res.send("Response from the GET request")
-});app.listen(3001);
-
-console.debug("Test NodeJS !");
